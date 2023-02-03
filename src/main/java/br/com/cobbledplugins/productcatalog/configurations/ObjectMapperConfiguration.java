dfe98c1ba5413b03eea2431198805bfe88e327d1
Henry Santos <henry.santos@viewb.com.br>
2023-02-02 21:16:51 -0300

    style: set json property naming strategy to snake case

--- src/main/java/br/com/cobbledplugins/productcatalog/configurations/ObjectMapperConfiguration.java
+++ src/main/java/br/com/cobbledplugins/productcatalog/configurations/ObjectMapperConfiguration.java
@@ -2,6 +2,7 @@ package br.com.cobbledplugins.productcatalog.configurations;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class ObjectMapperConfiguration {
   @Autowired
   public void customize(ObjectMapper objectMapper) {
     objectMapper
+      .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
       .configOverride(BigDecimal.class).setFormat(JsonFormat.Value.forShape(JsonFormat.Shape.STRING));
   }
 
