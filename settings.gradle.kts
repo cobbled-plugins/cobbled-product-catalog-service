2058160d43b18b7c8d4fa3f92b18d2e6937bcf3e
Henry Santos <henry.santos@viewb.com.br>
2023-02-05 12:59:55 -0300

    ci: add continuous-integration.yaml workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+  `gradle-enterprise`
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
+
 rootProject.name = "cobbled-product-catalog-service"
